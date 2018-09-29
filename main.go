dccf48b4f178f3db88a5541eaeacedb4cc5c627c
lalyos <lalyos@yahoo.com>
2018-09-29 15:31:12 +0200

    add usage and eval flags

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"github.com/miekg/dns"
 )
@@ -34,14 +35,43 @@ func handleRequest(w dns.ResponseWriter, r *dns.Msg) {
 	}
 }
 
+const EVAL = `
+set -x
+: === configure DNS:
+sudo mkdir -p /etc/resolver; echo -e 'nameserver 127.0.0.1\nport 5300\n'|sudo tee /etc/resolver/dev
+
+: === restart container:
+docker rm -f devdns; docker run -d --name devdns -p 5300:5300/udp lalyos/devdns -addr 0.0.0.0:5300
+
+: === test it
+ping -c 3 whatever.dev
+set +x
+`
+const USAGE = `
+devdns is a DNS server that replies 127.0.0.1 always
+full docs: https://github.com/lalyos/devdns
+
+oneliner on OSX:
+  eval "$(docker run --rm lalyos/devdns --eval)"
+`
+
 func main() {
 	var addr = flag.String("addr", "127.0.0.1:5300", "listen address")
 	var ip = flag.String("ip", "127.0.0.1", "resolve ipv4 address")
-
+	var usage = flag.Bool("usage", false, "prints docker usage information")
+	var eval = flag.Bool("eval", false, "generates script to be shell 'eval' -ed")
 	flag.Parse()
 
 	resolveIP = net.ParseIP(*ip)
 
+	if *usage {
+		fmt.Println(USAGE)
+		os.Exit(0)
+	}
+	if *eval {
+		fmt.Println(EVAL)
+		os.Exit(0)
+	}
 	if resolveIP == nil {
 		log.Fatalf("Invalid ip address: %s\n", *ip)
 	}
